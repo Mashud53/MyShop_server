16c319da9a2b9fc1655ac9f100594d0f4026e26e
unknown <almashud53@gmail.com>
2024-05-29 03:06:17 +0600

    order

--- index.js
+++ index.js
@@ -509,6 +509,14 @@ async function run() {
 
       res.send(result)
     })
+    app.delete ('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('delete order====', id)
+      const query = { _id: new ObjectId(id) }
+      const result = await ordersCollection.deleteOne(query);    
+
+      res.send(result)
+    })
 
     app.post('/cartOrders', async (req, res) => {
       const order = req.body;
